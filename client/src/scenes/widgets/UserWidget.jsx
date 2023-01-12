3bf9bbdb3e3aa3f937d126668f9ed6db3545d0d3
Guocheng Li <56323719+lilao8@users.noreply.github.com>
2023-01-11 22:31:12 -0500

    First completion

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -18,7 +18,7 @@ const UserWidget = ({ userId, picturePath }) => {
   const navigate = useNavigate();
   const token = useSelector((state) => state.token);
   const dark = palette.neutral.dark;
-  const mediu = palette.neutral.medium;
+  const medium = palette.neutral.medium;
   const main = palette.neutral.main;
 
   const getUser = async () => {
@@ -68,12 +68,85 @@ const UserWidget = ({ userId, picturePath }) => {
                   cursor: "pointer"
                 }
               }}>
-              {fristName} {lastName}
+              {firstName} {lastName}
             </Typography>
-            <Typography color={{medium}}>{friends.length}</Typography>
+            <Typography color={medium}>{friends.length}</Typography>
           </Box>
         </FlexBetween>
+        <ManageAccountsOutlined />
       </FlexBetween>
+
+        <Divider />
+
+        {/*SECOND ROW*/}
+        <Box
+          p="1rem 0"
+        >
+          <Box display="flex" alignItems="center" gap="1rem" mb="0.5rem">
+            <LocationOnOutlined fontSize="large" sx={{ color: main }} />
+            <Typography color={medium}>{location}</Typography>
+          </Box>
+          <Box display="flex" alignItems="center" gap="1rem">
+            <WorkOutlineOutlined fontSize="large" sx={{ color: main }} />
+            <Typography color={medium}>{occupation}</Typography>
+          </Box>
+        </Box>
+
+      <Divider />
+
+      {/*THIRD ROW*/}
+        <Box p="1rem 0">
+          <FlexBetween mb="0.5rem">
+            <Typography color={medium}>Who's viewed your profile</Typography>
+            <Typography color={medium} fontWeight="500">
+              {viewedProfile}
+            </Typography>
+          </FlexBetween>
+          <FlexBetween mb="0.5rem">
+            <Typography color={medium}>Impressions of your post</Typography>
+            <Typography color={medium} fontWeight="500">
+              {impressions}
+            </Typography>
+          </FlexBetween>
+        </Box>
+
+      <Divider />
+
+      {/*FORTH ROW*/}
+        <Box p="1rem 0">
+          <Typography fontWeight="500" color={main} fontSize="1rem" mb="1rem">
+            Social Profiles
+          </Typography>
+
+          <FlexBetween gap="1rem" mb="0.5rem">
+            <FlexBetween gap="1rem">
+              <img src="../assets/twitter.png" alt="twitter" />
+              <Box>
+                <Typography color={main} fontWeight="500">
+                  Twitter
+                </Typography>
+                <Typography color={medium}>Social Network</Typography>
+              </Box>
+            </FlexBetween>
+            <EditOutlined sx={{ color: main }} />
+          </FlexBetween>
+
+          <FlexBetween gap="1rem">
+            <FlexBetween gap="1rem">
+              <img src="../assets/linkedin.png" alt="linkedin" />
+              <Box>
+                <Typography color={main} fontWeight="500">
+                  Linkedin
+                </Typography>
+                <Typography color={medium}>Network Platform</Typography>
+              </Box>
+            </FlexBetween>
+            <EditOutlined sx={{ color: main }} />
+          </FlexBetween>
+
+        </Box>
     </WidgetWrapper>
   )
 };
+
+export default UserWidget;
